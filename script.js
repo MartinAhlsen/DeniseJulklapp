06fc1a7d22a7c9c47ec3da8f51053084e166cb4f
MartinAhlsen <martin.ahlsen@gmail.com>
2023-12-24 01:15:00 +0100

    working fade

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".card");
+$(document).ready(function () {
+  const card1 = $(".card-1");
+  const card2 = $(".card-2");
+  const passwordInput = $("#password-input");
 
-  cards.forEach((card) => {
-    card.addEventListener("click", function () {
-      // Prompt for password
-      const password = prompt("Enter the password:");
+  $("#password-submit").click(function () {
+    const password = passwordInput.val();
 
-      // Check if the password is correct
-      if (password === "1") {
-        // Replace 'yourpassword' with your actual password
-        // Rotate the card 180 degrees
-        card.classList.toggle("flipped");
-      } else {
-        alert("Incorrect password. Try again.");
-      }
-    });
+    // Check if the entered password is correct
+    if (password === "Vintermys") {
+      card1.addClass("fade-out");
+      setTimeout(function () {
+        card1.css("display", "none");
+        card2.css("display", "flex"); // Ensure card-2 is displayed before fading in
+        setTimeout(function () {
+          card2.addClass("fade-in");
+        }, 50); // A small delay before adding the fade-in class
+      }, 500); // Adjust the timing to match your transition duration
+    } else {
+      // Handle incorrect password (you can add your own logic here)
+      console.log("Incorrect password");
+    }
   });
 });
